0c16b31a6b22b5451b62eb63c67d75ea58a77977
Md. Golam Maula Maruf <37345164+Gm-Maruf@users.noreply.github.com>
2018-11-26 12:56:47 +0600

    Selling Tickets

---
+++ Quiz 6/Ticket Selling/TicketSellerTest.java
@@ -0,0 +1,15 @@
+package decisionsloops;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class TicketSellerTest {
+	TicketSeller ts = new TicketSeller();
+
+	@Test
+	void test() {
+		assertEquals(1,ts.calculation());
+	}
+
+}
